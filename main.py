7fbeeca94f9f3a79e652304536dabce04ad2bafa
ErIv31 <69343908+ErIv31@users.noreply.github.com>
2022-10-11 17:10:14 +0300

    Update main.py
    Разнес калькулятор времени и сбор температурных значений. 
    Добавил возможность проверки разметки по fdisk -l для определения раздела, который необходимо примонтировать.

--- main.py
+++ main.py
@@ -30,13 +30,15 @@ def format_disk(disk):
 
 
 # Примонтируем диск
-# def mount_disk(disk):
-#     os.system(f'mount /dev/{disk}1 /mnt/disk/')
+def mount_disk(disk):
+    os.system('mkdir /mnt/disk')
+    linux_filesystem_disk = get_console_output('fdisk -l | grep "Linux filesystem"')[0:9]
+    os.system(f'mount {linux_filesystem_disk} /mnt/disk/')
 
 
 # Перемещаемся в директорию диска
-# def move_to_disk_folder():
-#     os.system('cd /mnt/disk/; pwd')
+def move_to_disk_folder():
+    os.system('cd /mnt/disk/; pwd')
 
 
 # Запускаем стресс тест в фоновом режиме
@@ -46,15 +48,20 @@ def start_stress_test(test_time: str):
     os.system(f'stress-ng --sequential 0 --class io --timeout {test_time}s --metrics-brief &')
     print(f'Test is starting during {test_time} seconds')
 
-
-#  Получаем значения температур
-def temperature_list(test_time):
-    value_list = []
+    
+# Калькулятор времени
+def time_calculator(test_time):
     cores = get_console_output('nproc')
     time_end = time.time() + (float(test_time) * float(cores) + 10)
     time_format = time.strftime("%H:%M:%S", time.gmtime(time_end))
     print('Начало теста:', datetime.datetime.now().strftime("%H:%M:%S"))
     print('Конец теста:', time_format)
+    return time_end    
+    
+
+#  Получаем значения температур
+def temperature_list(test_time):
+    value_list = []
     while time.time() <= time_end:
         temperature_value = get_console_output(
             "smartctl -A /dev/sda | grep Temperature | awk '{print $10}'")
@@ -66,17 +73,19 @@ def temperature_list(test_time):
 
 if __name__ == "__main__":
     disks = get_console_output('ls -l /dev/disk/by-id')
-    logic_disk_name = get_disk_logical_name(disks)
-    amount_of_disk_partition = disk_amount(logic_disk_name)
-    if amount_of_disk_partition <= 1:
-        os.system(f'echo "g\nn\np\n1\n\n\nw" | fdisk /dev/{logic_disk_name}; mkfs.ext4 /dev/{logic_disk_name}1')
-    else:
-        mount_disk(logic_disk_name)
+    logic_disk_name = get_logical_disk_name(disks)
+    amount_of_disk_partition = how_many_partitions(logic_disk_name)
+    while amount_of_disk_partition <= 1:
+        format_disk(logic_disk_name)
+        #os.system(f'echo "g\nn\np\n1\n\n\nw" | fdisk /dev/{logic_disk_name}; mkfs.ext4 /dev/{logic_disk_name}1')
+        amount_of_disk_partition = how_many_partitions(logic_disk_name)
+    if amount_of_disk_partition > 1:
+        mount_disk()
         move_to_disk_folder()
         print('Укажи время теста в секундах')
         test_time = input()
         start_stress_test(test_time)
-        temperature_values = temperature_list(test_time)
+        temperature_values = temperature_list(time_calculator(test_time))
         #print(temperature_values)
         print('Минимальная температура: ', min(
             temperature_values), '°C', sep='')
